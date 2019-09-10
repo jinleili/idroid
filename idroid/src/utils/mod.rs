642c6766bb8f0da4a0b88bfff47615c9c25b175e
li jinlei <grenlight@icloud.com>
2019-09-10 14:07:40 +0800

    Fix staging buffer usage

--- idroid/src/utils/mod.rs
+++ idroid/src/utils/mod.rs
@@ -123,7 +123,7 @@ where
     T: 'static + Copy,
 {
     let staging_buffer = device
-        .create_buffer_mapped(1, wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST)
+        .create_buffer_mapped(1, wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_SRC)
         .fill_from_slice(&[uniforms]);
     let uniform_buffer = empty_uniform_buffer(device, size);
     encoder.copy_buffer_to_buffer(&staging_buffer, 0, &uniform_buffer, 0, size);
