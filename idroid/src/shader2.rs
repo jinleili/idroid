0b3061c665f9299da2b2aed5ba241342069823b9
LiJinlei <grenlight@icloud.com>
2021-04-07 16:46:40 +0800

    fn create_shader_module for iOS TARGET

--- idroid/src/shader2.rs
+++ idroid/src/shader2.rs
@@ -2,6 +2,8 @@ use std::{borrow::Cow, fs::read_to_string, path::PathBuf};
 use wgpu::util::DeviceExt;
 use wgpu::{ShaderFlags, ShaderModule, ShaderModuleDescriptor, ShaderSource};
 
+#[cfg(not(target_os = "ios"))]
+#[allow(dead_code)]
 pub fn create_shader_module(device: &wgpu::Device, shader_name: &'static str, lable: Option<&str>) -> ShaderModule {
     // @Kvark 20210402 ：Please don't use EXPERIMENTAL_TRANSLATION on Metal for this shader for now.
     // let flags = ShaderFlags::VALIDATION | ShaderFlags::EXPERIMENTAL_TRANSLATION;
@@ -29,6 +31,30 @@ pub fn create_shader_module(device: &wgpu::Device, shader_name: &'static str, la
     })
 }
 
+#[cfg(target_os = "ios")]
+#[allow(dead_code)]
+pub fn create_shader_module(device: &wgpu::Device, shader_name: &'static str, lable: Option<&str>) -> ShaderModule {
+    // @Kvark 20210402 ：Please don't use EXPERIMENTAL_TRANSLATION on Metal for this shader for now.
+    // let flags = ShaderFlags::VALIDATION | ShaderFlags::EXPERIMENTAL_TRANSLATION;
+    let flags = ShaderFlags::VALIDATION;
+
+    // env!("CARGO_MANIFEST_DIR") 是编译时执行的，得到的是当前所编辑的库的所在路径，而不是项目的路径
+    let base_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let path = PathBuf::from(&base_dir).join("shader-preprocessed-wgsl").join(format!("{}.wgsl", shader_name));
+    let code = match read_to_string(&path) {
+        Ok(code) => code,
+        Err(e) => {
+            panic!("Unable to read {:?}: {:?}", path, e)
+        }
+    };
+
+    device.create_shader_module(&ShaderModuleDescriptor {
+        label: lable,
+        source: ShaderSource::Wgsl(Cow::Borrowed(&code)),
+        flags,
+    })
+}
+
 const SHADER_IMPORT: &str = "#include ";
 
 #[allow(dead_code)]
@@ -47,18 +73,20 @@ fn parse_shader_source(source: &str, output: &mut String, base_path: &str) {
                 }
             }
         } else {
+            output.push_str(line);
+            output.push_str("\n");
             // 移除注释
-            let need_delete = match line.find("//") {
-                Some(_) => {
-                    let segments: Vec<&str> = line.split("//").collect();
-                    segments.len() > 1 && segments.first().unwrap().trim().is_empty()
-                }
-                None => false,
-            };
-            if !need_delete {
-                output.push_str(line);
-                output.push_str("\n");
-            }
+            // let need_delete = match line.find("//") {
+            //     Some(_) => {
+            //         let segments: Vec<&str> = line.split("//").collect();
+            //         segments.len() > 1 && segments.first().unwrap().trim().is_empty()
+            //     }
+            //     None => false,
+            // };
+            // if !need_delete {
+            //     output.push_str(line);
+            //     output.push_str("\n");
+            // }
         }
     }
 }
